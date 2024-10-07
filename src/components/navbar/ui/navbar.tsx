2b505ec4be9b1220243809c1827020fb40d3527f
RichardSimmons <simmons.105@wright.edu>
2024-10-06 22:40:06 -0400

    Update navbar.tsx

--- src/components/navbar/ui/navbar.tsx
+++ src/components/navbar/ui/navbar.tsx
@@ -2,20 +2,21 @@
 
 import Link from "next/link";
 import { Logo } from "~/components/ui/logo";
-import { NumberTicker } from "~/components/ui/number-ticker";
-import { IoLogoGithub } from "react-icons/io";
-import { FaStar } from "react-icons/fa";
-import { useGitHubRepoData } from "~/hooks/useGitHubRepoData";
+// import { NumberTicker } from "~/components/ui/number-ticker";
+// import { IoLogoGithub } from "react-icons/io";
+// import { FaStar } from "react-icons/fa";
+// import { useGitHubRepoData } from "~/hooks/useGitHubRepoData";
 
 export function Navbar() {
-  const repoData = useGitHubRepoData("yoyocharlie", "nextMotion");
+  // const repoData = useGitHubRepoData("yoyocharlie", "nextMotion");
   return (
     <nav className="sticky left-0 top-0 z-50 flex w-full items-center bg-[#000000ab] px-5 backdrop-blur-lg">
       <div className="m-auto flex w-full max-w-[1800px] items-center justify-between py-2">
         <Link href="/" className="inline-block">
           <Logo className="w-28" />
         </Link>
-        <Link
+        {/* Remove the GitHub link */}
+        {/* <Link
           className="group flex items-center gap-3 transition-all duration-300 hover:brightness-125"
           target="_blank"
           href={repoData.url}
@@ -23,7 +24,7 @@ export function Navbar() {
           <div className="flex items-center gap-1 rounded-[10px_5px_10px_5px] bg-accent px-2 py-1">
             <IoLogoGithub size={20} />
             <span className="hidden text-sm font-light sm:flex">
-              Star us on GitHub
+              GitHub
             </span>
           </div>
           <div className="flex w-12 items-center gap-1">
@@ -36,7 +37,7 @@ export function Navbar() {
               value={repoData.starCount}
             />
           </div>
-        </Link>
+        </Link> */}
       </div>
     </nav>
   );
