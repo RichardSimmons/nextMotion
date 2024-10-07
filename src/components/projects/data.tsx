4727662098a4fe0ebc6a75ccb427c3f983f065f5
RichardSimmons <simmons.105@wright.edu>
2024-10-07 11:22:46 -0400

    Update data.tsx

--- src/components/projects/data.tsx
+++ src/components/projects/data.tsx
@@ -42,11 +42,11 @@ export const timelineData: TimelineEntry[] = [
     title: "Music Generator using diffusion models",
     projectUrls: {
       site: {
-        url: "https://github.com/RSimmons2021/music-generation-audio-diffusion",
+        //url: "https://github.com/RSimmons2021/music-generation-audio-diffusion/tree/main/Desktop/New%20folder/music-generation-audio-diffusion",
         icon: <RiExternalLinkLine size={20} />,
       },
       repo: {
-        name: "music-generation-audio-diffusion",
+        name: "music-generation-audio-diffusion/tree/main/Desktop/New%20folder/music-generation-audio-diffusion",
         owner: "RSimmons2021",
         showStarCount: false,
       },
