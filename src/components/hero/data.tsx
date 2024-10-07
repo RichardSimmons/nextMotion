ce66448bbb1fc5eebf7fd464c59645adfc1da13e
RichardSimmons <simmons.105@wright.edu>
2024-10-06 20:46:21 -0400

    Update data.tsx

--- src/components/hero/data.tsx
+++ src/components/hero/data.tsx
@@ -11,7 +11,7 @@ export const title = {
 export const profileCard = {
   title: "Richard Simmons",
   subTitle: "Fullstack Software Developer",
-  body: `Passionate about building scalable, user-friendly web applications. Always exploring new technologies and best practices to improve development processes. And I like cats.`,
+  body: `Perfection lies in mastering the small things with precision. As a Full Stack engineer, I’m on a constant journey to discover the perfect intersection of science and art in technology. Continuously exploring new innovations and best practices, I strive to refine development processes and push the boundaries of what's possible. And in moments of pause, I find joy in the simple wonders of life—like koala bears, a reminder of balance in a world of constant advancement.`,
   socialUrls: [
     {
       url: "https://www.linkedin.com/in/richard-simmons-a3916958/",
